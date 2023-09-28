58757fc10ef51243a2988722a606c2c13a576a6a
Oyelola Adeboye <78259921+Boye-dev@users.noreply.github.com>
2023-09-28 10:17:33 +0100

    update url

--- src/api/api.js
+++ src/api/api.js
@@ -6,7 +6,8 @@ import { getToken, isAuthenticated, removeToken } from "../utils/auth";
 let refreshed = false;
 
 export const baseUrl =
-  process.env.REACT_APP_API_BASE_URL || "http://13.48.194.188/api";
+  process.env.REACT_APP_API_BASE_URL ||
+  "https://femmetech-backend.onrender.com/api";
 // "http://localhost:8080/api";
 
 // "http://localhost:4000/api/v1";
