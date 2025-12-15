bddda9c1446365ea9a818aae44e09a29015d3d04
Oyelola Adeboye <78259921+Boye-dev@users.noreply.github.com>
2025-12-15 18:42:55 +0100

    fix: updated scoket

--- src/context/FemmetechContext.js
+++ src/context/FemmetechContext.js
@@ -10,7 +10,7 @@ const FemmetechContext = createContext({});
 const FemmetechContextProvider = ({ children }) => {
   const ENDPOINT = process.env.BACKEND_URL;
   const { socketRef, message, typing, inAppNotification, latestMessage } =
-    useSocket("https://femmetech-backend.onrender.com");
+    useSocket("https://femmetech-api.boye-dev.com");
   const [openNotification, setOpenNotification] = useState(false);
 
   useEffect(() => {
